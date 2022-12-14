06f03887162b599db75dfff11ee63cf0251ab894
Zen Software <ZenSoftware@users.noreply.github.com>
2022-12-14 01:42:46 -0700

    formatting

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { fromEvent, switchMap, interval } from 'rxjs';
 
 const sampleButton = document.querySelector<HTMLButtonElement>('#sample-button')!;
 const output = document.querySelector<HTMLDivElement>('#output')!;
-
 const clearButton = document.querySelector<HTMLButtonElement>('#clear-button')!;
+
 clearButton.addEventListener('click', () => (output.innerHTML = ''));
 
 fromEvent(sampleButton, 'click')
